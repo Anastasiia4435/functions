363c7b8d6a4ff0e700d15420717ecd5ad9ef6751
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-03-28 16:59:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function numbers(){
+    for(let i = 3; i <= 13; i++){
+        console.log(i);
+    }
+}
+
+numbers();
+
+function number10(){
+    for(let i = 0; i < 10; i++) {
+        console.log(10);
+    }
+}
+
+number10();
+
+function twoNumbers(a, b) {
+    if (a > b){
+        console.log(a);
+    }
+    else {
+        console.log(b);
+    }
+}
+
+twoNumbers(15, 30);
